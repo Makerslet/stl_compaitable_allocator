909bdc19cacab4430e9cac5a7ce2301d22e35a9e
Makerslet <oshuev.valery@yandex.ru>
2020-02-10 09:22:30 +0300

    базовая реализация односвязного списка без использования итератора

--- custom_forward_list.h
+++ custom_forward_list.h
@@ -10,18 +10,23 @@ class custom_forward_list {
     struct list_node {
         T object;
         list_node* next = nullptr;
+
+        list_node() {}
+        list_node(const T& value) : object(value) {}
     };
 
     using node_ptr = list_node*;
 
 public:
 
+    // TODO: solve const/non const versions
     class iterator : public std::iterator<
             std::forward_iterator_tag, T> {
 
         using value_type = T;
         using pointer_type = T*;
         using reference_type = T&;
+    public:
 
         iterator(node_ptr node) : _current_node(node){
         }
@@ -50,18 +55,101 @@ public:
         node_ptr _current_node;
     };
 
+    class const_iterator : public std::iterator<
+            std::forward_iterator_tag, T> {
+
+        using value_type = const T;
+        using pointer_type = const T*;
+        using reference_type = const T&;
+    public:
+
+        const_iterator(node_ptr node) : _current_node(node){
+        }
+        iterator operator++() {
+            iterator tmp = *this;
+            _current_node = _current_node->next;
+            return tmp;
+        }
+        iterator operator++(int) {
+            _current_node = _current_node->next;
+            return *this;
+        }
+        T& operator*() {
+            return _current_node->object;
+        }
+        T* operator->() {
+            return &_current_node->object;
+        }
+        bool operator==(const iterator& rhs) {
+            return _current_node == rhs._current_node;
+        }
+        bool operator!=(const iterator& rhs) {
+            return  _current_node != rhs._current_node;
+        }
+    private:
+        node_ptr _current_node;
+    };
+
+    T& front() {
+        return static_cast<T&>(
+                    const_cast<custom_forward_list<T>*>(this)->front());
+    }
+
+    const T& front() const {
+        if(empty())
+            throw std::range_error("custom_forward_list is epmty");
+
+        return _begin->object;
+    }
+
     custom_forward_list() :_size(0)  {
-        list_node tmp = new list_node();
+        node_ptr tmp = new list_node();
         _begin = tmp;
         _end = tmp;
     }
 
+    // TODO: implement move push front
+    // TODO implement emplace_push_front
     void push_front(const T& value) {
+        node_ptr new_front = new list_node(value);
+        new_front->next = _begin;
+        _begin = new_front;
+        ++_size;
+    }
 
+    void pop_front() {
+        if(empty())
+            return;
+
+        node_ptr tmp = _begin;
+        _begin = tmp->next;
+
+        delete tmp;
+        --_size;
     }
 
     bool empty() const {
+        return _begin == _end;
+    }
 
+    std::size_t size() const {
+        return _size;
+    }
+
+    iterator begin() {
+        return iterator(_begin);
+    }
+
+    iterator end() {
+        return  iterator(_end);
+    }
+
+    iterator cbegin() const {
+        return const_iterator(_begin);
+    }
+
+    iterator cend() const {
+        return  const_iterator(_end);
     }
 
 private:
--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include "allocator.h"
+#include "custom_forward_list.h"
 #include <map>
 
 int main(int, char *[]) {
 
+    /*
      std::map<int, int, std::less<int>,
          preventive_allocator<std::pair<const int, int>, 10>> map;
 
      for (int i = 0; i < 8; ++i) {
          map[i] = i;
          std::cout << std::endl;
-     }
+     }*/
+
+    custom_forward_list<int> cfl;
+    for(int i = 0; i < 10; ++i)
+        cfl.push_front(i);
+
+    for(const int& value : cfl)
+        std::cout << value << " " << cfl.size() << std::endl;
+
+    while(!cfl.empty()) {
+        cfl.pop_front();
+        std::cout << cfl.size() << std::endl;
+    }
 
     return 0;
 }
